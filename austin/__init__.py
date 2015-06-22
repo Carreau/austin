50b753c2802b1908e5968e153985cd1287d83a5c
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-06-22 23:37:29 +0200

    SciPy: chalenge accepted

---
+++ austin/__init__.py
@@ -0,0 +1,9 @@
+"""
+austin scipy package 
+
+see scipy 2015 tshirt
+"""
+
+__version__ = '2015.0.0'
+
+from . import scipy
---
+++ austin/scipy.py
@@ -0,0 +1,5 @@
+import datetime as _datetime
+
+date = _datetime.date(2015, 7, 6)
+
+
