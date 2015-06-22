7090eb86a80c620ca05b07d4913cfb8dd5416776
Thomas Kluyver <takowl@gmail.com>
2015-06-22 15:19:58 -0700

    More info about conference

--- austin/scipy.py
+++ austin/scipy.py
@@ -2,4 +2,27 @@ import datetime as _datetime
 
 date = _datetime.date(2015, 7, 6)
 
+url = "http://scipy2015.scipy.org/"
 
+code_of_conduct = "http://www.scipy2015.scipy.org/ehome/115969/259274/?&"
+
+location = "AT&T Executive Education and Conference Center, Austin, TX, USA"
+climate = "Humid subtropical"
+
+sections = [
+    {'name': 'tutorials',
+     'start': _datetime.date(2015, 7, 6),
+     'end': _datetime.date(2015, 7, 7),
+     'url': 'http://www.scipy2015.scipy.org/ehome/115969/289057/?&',
+    },
+    {'name': 'talks',
+     'start': _datetime.date(2015, 7, 8),
+     'end': _datetime.date(2015, 7, 10),
+     'url': 'http://www.scipy2015.scipy.org/ehome/115969/297898/?&',
+    },
+    {'name': 'sprints',
+     'start': _datetime.date(2015, 7, 11),
+     'end': _datetime.date(2015, 7, 12),
+     'url': 'http://www.scipy2015.scipy.org/ehome/115969/292867/?&',
+    },
+]
