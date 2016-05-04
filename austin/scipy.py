fbfa8d610e5acf17ac228319918056d264789c1e
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-05-04 12:12:02 -0700

    'update for scipy 2016'

--- austin/scipy.py
+++ austin/scipy.py
@@ -1,28 +1,28 @@
 import datetime as _datetime
 
-date = _datetime.date(2015, 7, 6)
+date = _datetime.date(2016, 7, 6)
 
-url = "http://scipy2015.scipy.org/"
+url = "http://scipy2016.scipy.org/"
 
-code_of_conduct = "http://www.scipy2015.scipy.org/ehome/115969/259274/?&"
+code_of_conduct = "http://scipy2016.scipy.org/ehome/146062/388087/"
 
 location = "AT&T Executive Education and Conference Center, Austin, TX, USA"
 climate = "Humid subtropical"
 
 sections = [
     {'name': 'tutorials',
-     'start': _datetime.date(2015, 7, 6),
-     'end': _datetime.date(2015, 7, 7),
-     'url': 'http://www.scipy2015.scipy.org/ehome/115969/289057/?&',
+     'start': _datetime.date(2016, 7, 11),
+     'end': _datetime.date(2016, 7, 12),
+     'url': 'http://scipy2016.scipy.org/ehome/146062/332960/',
     },
     {'name': 'talks',
-     'start': _datetime.date(2015, 7, 8),
-     'end': _datetime.date(2015, 7, 10),
-     'url': 'http://www.scipy2015.scipy.org/ehome/115969/297898/?&',
+     'start': _datetime.date(2016, 7, 13),
+     'end': _datetime.date(2016, 7, 15),
+     'url': 'http://scipy2016.scipy.org/ehome/146062/332968/',
     },
     {'name': 'sprints',
-     'start': _datetime.date(2015, 7, 11),
-     'end': _datetime.date(2015, 7, 12),
-     'url': 'http://www.scipy2015.scipy.org/ehome/115969/292867/?&',
+     'start': _datetime.date(2016, 7, 16),
+     'end': _datetime.date(2016, 7, 17),
+     'url': 'http://scipy2016.scipy.org/ehome/146062/332969/',
     },
 ]
